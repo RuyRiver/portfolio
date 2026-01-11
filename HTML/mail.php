12a838619ef4070de9e4589c9b97d022ef8b9681
RuyRiver <ruyriverdm@gmail.com>
2026-01-11 19:49:43 +0100

    feat: fully identity migrantion; replace further instances  "Ruy River" with "Rodrigo Rivas"

--- HTML/mail.php
+++ HTML/mail.php
@@ -1,15 +1,15 @@
 <?php
 
-if ( $_POST ) {
+if ($_POST) {
 
-	$frm_name  = 'Ruy River'; // your name
-	$recepient = 'hey@ruyriver.com'; // your e-mail
-	$sitename  = 'Ruy River - UI/UX Portfolio'; // your site name
-	$subject   = "Message from \"$sitename\""; // subject template
+	$frm_name = 'Rodrigo Rivas'; // your name
+	$recepient = 'hello@rodrigorivas.co'; // your e-mail
+	$sitename = 'Rodrigo Rivas - Portfolio'; // your site name
+	$subject = "Message from \"$sitename\""; // subject template
 
-	$name  = trim( $_POST['visitor_name'] );
-	$email = trim( $_POST['visitor_email'] );
-	$msg   = trim( $_POST['visitor_msg'] );
+	$name = trim($_POST['visitor_name']);
+	$email = trim($_POST['visitor_email']);
+	$msg = trim($_POST['visitor_msg']);
 
 	$message = "
 	-------------------<br><br>
@@ -19,6 +19,6 @@ if ( $_POST ) {
 	<br><br>-------------------
 	";
 
-	mail( $recepient, $subject, $message, "From: $name <$email>" . "\r\n" . "Reply-To: $email" . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'Content-type: text/html; charset="utf-8"' );
+	mail($recepient, $subject, $message, "From: $name <$email>" . "\r\n" . "Reply-To: $email" . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'Content-type: text/html; charset="utf-8"');
 
 }
